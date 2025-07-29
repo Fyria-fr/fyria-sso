ce89f623c9f26fe932ab530270e4e2023ff5b8b1
RomainEntreCodeurs <romain@lesentrecodeurs>
2025-07-29 17:12:46 +0200

    feat: add i18n

--- apps/web/src/app/routes/app/app.tsx
+++ apps/web/src/app/routes/app/app.tsx
@@ -1,3 +1,6 @@
+import { useTranslation } from 'react-i18next';
+
 export const AppRoute = () => {
-  return <div>test</div>;
+  const { t } = useTranslation();
+  return <div>{t('welcome')}</div>;
 };
--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 
 import './index.css';
 import { App } from './app';
+import './i18n';
 
 const root = document.getElementById('root');
 if (!root) throw new Error('No root element found');
